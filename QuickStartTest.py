1dc2f43f69bb76afa30eb136852a79cabed9bb75
Cetus <purpleinklingyu@gmail.com>
2020-08-18 19:18:48 -0400

    initial commit (test)

--- QuickStartTest.py
+++ QuickStartTest.py
@@ -13,16 +13,49 @@ def main():
     client = language.LanguageServiceClient()
 
     # The text to analyze
-    text = u'Hello, world!'
+    text = u' Knowledge of one or more of the following technologies is required: C, C++, C#, .NET, Java, Angular, Web Technologies, Xamarin, mobile technologies, design patterns, SQL, Database technologies, XML, Perl, Python, MatLab  Familiarity or interest in one or more of the following areas: Agile, Scrum, DevOps, Continuous Integration/Continuous Development (CI/CD) Pipeline, Version Control, TFS, Azure DevOps, GitHub, Product documentation, Process Documentation  Strong verbal/written communication skills  Prior work on projects in a team environment preferred  Relevant academic projects, internship or lab experience preferred '
+    text = text.encode("ascii", "ignore")
     document = types.Document(
         content=text,
         type=enums.Document.Type.PLAIN_TEXT)
 
     # Detects the sentiment of the text
-    sentiment = client.analyze_sentiment(document=document).document_sentiment
+    encoding_type = enums.EncodingType.UTF8
+    sentiment = client.analyze_entities(document=document, encoding_type=encoding_type)
 
+    # Loop through entitites returned from the API
+    for entity in sentiment.entities:
+        print(u"Representative name for the entity: {}".format(entity.name))
+
+        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+        print(u"Entity type: {}".format(enums.Entity.Type(entity.type).name))
+
+        # Get the salience score associated with the entity in the [0, 1.0] range
+        print(u"Salience score: {}".format(entity.salience))
+
+        # Loop over the metadata associated with entity. For many known entities,
+        # the metadata is a Wikipedia URL (wikipedia_url) and Knowledge Graph MID (mid).
+        # Some entity types may have additional metadata, e.g. ADDRESS entities
+        # may have metadata for the address street_name, postal_code, et al.
+        for metadata_name, metadata_value in entity.metadata.items():
+            print(u"{}: {}".format(metadata_name, metadata_value))
+
+        # Loop over the mentions of this entity in the input document.
+        # The API currently supports proper noun mentions.
+        for mention in entity.mentions:
+            print(u"Mention text: {}".format(mention.text.content))
+
+            # Get the mention type, e.g. PROPER for proper noun
+            print(
+                u"Mention type: {}".format(enums.EntityMention.Type(mention.type).name)
+            )
+
+    # Get the language of the text, which will be the same as
+    # the language specified in the request or, if not specified,
+    # the automatically-detected language.
+    print(u"Language of the text: {}".format(sentiment.language))
     print('Text: {}'.format(text))
-    print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+    #print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
 
 
 if __name__ == "__main__":
