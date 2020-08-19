309bf3cbcc1390ec9c4c3743adf87eda26787fe4
unknown <jeffzhao7@gmail.com>
2020-08-18 20:15:31 -0400

    updates

--- nlpTest.py
+++ nlpTest.py
@@ -1,40 +1,51 @@
 import bs4 as bs
 import urllib.request
-import codecs as codec
-#getting linkedin main job search page
-sauce = urllib.request.urlopen("https://www.linkedin.com/jobs/search/?keywords=computer%20science").read()
-soup = bs.BeautifulSoup(sauce,"lxml")
 
-print(soup.text.encode("utf-8"))
+def pullData():
+    #getting linkedin main job search page
+    sauce = urllib.request.urlopen("https://www.linkedin.com/jobs/search/?keywords=computer%20science").read()
+    soup = bs.BeautifulSoup(sauce,"lxml")
+    links = soup.find_all("a",{"class": "result-card__full-card-link"})
 
-links = soup.find_all("a",{"class": "result-card__full-card-link"})
+    jobs = []
+    #going through all the links
+    for i in links:
+        subSauce = urllib.request.urlopen(i["href"]).read()
+        subSoup = bs.BeautifulSoup(subSauce,"lxml")
 
-for i in links:
-    subSauce = urllib.request.urlopen(i["href"]).read()
-    subSoup = bs.BeautifulSoup(subSauce,"lxml")
+        company = subSoup.find("a",{"class": "topcard__org-name-link topcard__flavor--black-link"}).text.encode("utf-8")
+        jobTitle = subSoup.find("h2",{"class": "topcard__title"}).text.encode("utf-8")
+        desc = subSoup.find("div",{"class": "show-more-less-html__markup"})
+        descText = desc.text
+        list = []
+        for j in desc.find_all("u"):
+            descText = descText.replace(j.text,"")
+        for j in desc.find_all("ul"):
+            descText = descText.replace(j.text,"")
 
-    company = subSoup.find("a",{"class": "topcard__org-name-link topcard__flavor--black-link"})
-    print(company.text.encode("utf-8"))
-    jobTitle = subSoup.find("h2",{"class": "topcard__title"})
-    print(jobTitle.text.encode("utf-8"))
+        jobDict = {
+            "company": company,
+            "title": jobTitle,
+            "reqs": descText
+        }
 
-    print("\n")
+        jobs.append(jobDict)
 
-    desc = subSoup.find("div",{"class": "show-more-less-html__markup"})
-    descText = desc.text
-    list = []
-    for j in desc.find_all("u"):
-        descText = descText.replace(j.text,"")
-    for j in desc.find_all("ul"):
-        descText = descText.replace(j.text,"")
-        list.append(j.text)
+    return jobs
 
-    print("WITHOUT LIST")
-    print(descText.encode("utf-8"))
-    print("\n")
+#def cleanData(jobs):
+    #for i in jobs:
+        #i["reqs"] = i["reqs"].replace("."," ")
+        #i["reqs"] = i["reqs"].replace(","," ")
 
-    print("THE LIST")
-    for i in list:
-        print(i)
+    #return jobs
 
-    print("\n")
+def main():
+    jobs = pullData()
+    #jobs = cleanData(jobs)
+    for i in jobs:
+        print(i["reqs"].encode("utf-8"))
+
+
+if __name__ == "__main__":
+    main()
