dc06137f7be7bb81a27533040be9fcac4ef3b98f
unknown <jeffzhao7@gmail.com>
2020-08-19 01:09:23 -0400

    Update 4

--- nlpTest.py
+++ nlpTest.py
@@ -18,11 +18,11 @@ def pullLink():
 
         #grabbing company name
         company = subSoup.find("a",{"class": "topcard__org-name-link topcard__flavor--black-link"}).text
-        company = company.encode("ascii","ignore")
+        company = company.encode("ascii","ignore").decode('utf-8')
 
         #grabbing job title
         jobTitle = subSoup.find("h2",{"class": "topcard__title"}).text
-        jobTitle = jobTitle.encode("ascii", "ignore")
+        jobTitle = jobTitle.encode("ascii", "ignore").decode('utf-8')
 
         #grabbing date posted
         datePosted = subSoup.find("span",{"class": "topcard__flavor--metadata posted-time-ago__text"})
@@ -38,7 +38,7 @@ def pullLink():
 
         #grabbing location
         location = subSoup.find("span",{"class": "topcard__flavor topcard__flavor--bullet"}).text
-        location = location.encode("ascii","ignore")
+        location = location.encode("ascii","ignore").decode('utf-8')
 
         #grabbing desc
         desc = subSoup.find("div",{"class": "show-more-less-html__markup"})
@@ -49,7 +49,7 @@ def pullLink():
         for j in desc.find_all("ul"):
             descText = descText.replace(j.text,"")
 
-        descText = descText.encode("ascii","ignore")
+        descText = descText.encode("ascii","ignore").decode('utf-8')
 
         jobDict = {
             "company": company,
@@ -64,33 +64,92 @@ def pullLink():
     return jobs
 
 def pullMonster():
-    sauce = urllib.request.urlopen("https://www.monster.com/jobs/search/?q=Computer-Science&tm=14&jobid=219011797").read()
+    sauce = urllib.request.urlopen("https://www.monster.com/jobs/search/?q=Computer-Science&intcid=skr_navigation_nhpso_searchMainPrefill&tm=14").read()
     soup = bs.BeautifulSoup(sauce, "lxml")
     links = soup.find_all("h2", {"class": "title"})
 
+    jobs = []
+
     for i in links:
-        #obtaining information
+        #obtaining link
         link = i.find("a",href=True)
-        print(link['href'])
+        subSauce = urllib.request.urlopen(link['href']).read()
+        subSoup = bs.BeautifulSoup(subSauce, "lxml")
+
+        #grabbing company name
+        company = subSoup.find("div", {"class": "job_company_name tag-line c-primary"}).text
+        if(company is ""):
+            continue
+        else:
+            company = company.encode("ascii", "ignore").decode('utf-8')
+
+        # grabbing job title
+        jobTitle = subSoup.find("h1", {"class": "job_title c-primary-dk"}).text
+        jobTitle = jobTitle.encode("ascii", "ignore").decode('utf-8')
+
+        # grabbing date posted
+        datePosted = subSoup.find("div", {"name": "value_posted"})
+        if (datePosted.text == "Today"):
+            datePosted = datetime.datetime.now().date()
+        elif(datePosted.text == "30+ days ago"):
+            today = datetime.datetime.now()
+            d = datetime.timedelta(30)
+            datePosted = (today - d).date()
+        else:
+            splitTime = datePosted.text.split(" ")
+            today = datetime.datetime.now()
+            d = datetime.timedelta(int(splitTime[1]))
+            datePosted = (today - d).date()
+
+        # grabbing location
+        location = subSoup.find("div", {"name": "job_company_location"}).text
+        location = re.sub("\d", "", location)
+        location = location.encode("ascii", "ignore").decode('utf-8')
+
+        # grabbing desc
+        desc = subSoup.find("div", {"name": "job_description"})
+        descText = desc.text
+        for j in desc.find_all("u"):
+            descText = descText.replace(j.text, "")
+        for j in desc.find_all("ul"):
+            descText = descText.replace(j.text, "")
+            descText = descText + j.text
+
+        descText = descText.encode("ascii", "ignore").decode('utf-8')
+
+        jobDict = {
+            "company": company,
+            "title": jobTitle,
+            "reqs": descText,
+            "datePosted": datePosted,
+            "location": location
+        }
+
+        jobs.append(jobDict)
+
+    return jobs
+
+
 
 def cleanData(jobs):
     for i in jobs:
         i["reqs"] = i["reqs"].lower()
-        i["reqs"] = i["reqs"].replace("C++","cplusplus")
-        i["reqs"] = re.sub('[\W_]+'," ",i["reqs"])
+        i["reqs"] = i["reqs"].replace("C++", "cplusplus").encode("ascii", "ignore").decode('utf-8')
+        i["reqs"] = re.sub('[\W_]+', " ", i["reqs"]).encode("ascii", "ignore").decode('utf-8')
 
     return jobs
 
 def main():
-    pullLink()
     jobs = pullLink()
-    #jobs = cleanData(jobs)
-    for i in jobs:
-        print(i["company"])
-        print(i["title"])
-        print(i["reqs"])
-        print(i["datePosted"])
-        print(i["location"])
+    jobs2 = pullMonster()
+    jobs = jobs +jobs2
+    jobs = cleanData(jobs)
+    for i in jobs2:
+            print(i["company"])
+            print(i["title"])
+            print(i["reqs"])
+            print(i["datePosted"])
+            print(i["location"])
 
 if __name__ == "__main__":
     main()
