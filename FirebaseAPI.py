5cce110bf8e0e6ab8094dc4c11b041ecccdceb8c
Cetus <purpleinklingyu@gmail.com>
2020-08-18 20:11:54 -0400

    firebase insert

---
+++ FirebaseAPI.py
@@ -0,0 +1,15 @@
+from firebase import firebase
+import CREDENTIALS
+
+firebase = firebase.FirebaseApplication(CREDENTIALS.GOOGLE_FIREBASE, None)
+
+def insert(data):
+    #data =  { 'Name': 'John Doe',
+    #          'RollNo': 3,
+    #          'Percentage': 70.02
+    #          }
+    for x in data:
+        result = firebase.post(CREDENTIALS.GOOGLE_FIREBASE_TABLE,list)
+        print(result)
+
+
